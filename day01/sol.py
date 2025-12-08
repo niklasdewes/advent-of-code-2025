690c3ce08594db78bb98c261a1cc7191650366f9
Niklas Dewes <niklas.dewes@mailbox.org>
2025-12-08 17:15:51 +0100

    day 1

---
+++ day01/sol.py
@@ -0,0 +1,19 @@
+count_zero = 0
+pos = 50
+
+with open("input.txt") as f:
+    for line in f:
+        line = line.strip()
+        dir = line[0]
+        val = int(line[1:])
+
+        for _ in range(val):
+            if dir == "L":
+                pos = (pos-1) % 100
+            else: 
+                pos = (pos+1) % 100
+
+            if pos == 0:
+                count_zero += 1
+
+print(count_zero)
