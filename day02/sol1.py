5cdb816ec567c5e24f7cd3fbad5237560456b29d
Niklas Dewes <niklas.dewes@mailbox.org>
2025-12-11 23:49:10 +0100

    solve day02 part 1 & 2

---
+++ day02/sol1.py
@@ -0,0 +1,28 @@
+invalid_ids = 0
+
+with open("input.txt") as f:
+    for line in f:
+        line = line.split(",")
+        for ids in line:
+            start, end = ids.split("-")
+            for i in range(int(start), int(end) + 1):
+                if len(str(start)) % 2 == 0 or len(str(end)) % 2 == 0:
+                    s = str(i)
+                    n = len(s)
+                    for l in range(1, n // 2 + 1):
+                        if n % l == 0:
+                            m = s[:l]
+                            count_rep = n // l
+                            new_s = m * count_rep
+                            if new_s == s:
+                                s_split = len(s) // 2
+                                l_s = s[:s_split]
+                                r_s = s[s_split:]
+                                if l_s == r_s:
+                                    invalid_ids += int(s)   
+                                    break
+
+                else:
+                    pass
+
+print(invalid_ids)
---
+++ day02/sol2.py
@@ -0,0 +1,22 @@
+invalid_ids = 0
+
+with open("input.txt") as f:
+    for line in f:
+        line = line.split(",")
+        for ids in line:
+            start, end = ids.split("-")
+            for i in range(int(start), int(end) + 1):
+                s = str(i)
+                n = len(s)
+                for l in range(1, n // 2 + 1):
+                    if n % l == 0:
+                        m = s[:l]
+                        count_rep = n // l
+                        new_s = m * count_rep
+                        if new_s == s:
+                            invalid_ids += int(s)   
+                            break
+                else:
+                    pass
+
+print(invalid_ids)
